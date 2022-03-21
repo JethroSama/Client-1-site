4018ba3c52e56c428dd6555de43714a6dbf7090d
JethroSama <kel73896@gmail.com>
2022-03-21 19:15:17 +0800

    change active color to lighter shade of blue

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -4,7 +4,7 @@ import React from 'react'
 const HeaderItem = ({ text, Icon, active, url }) => {
     return (
         <Link href={ url }>
-            <div className={ `cursor-pointer flex items-center hover:text-blue-400 transition-colors ease-out space-x-1 group ${active && 'text-blue-500'}` }>
+            <div className={ `cursor-pointer flex items-center hover:text-blue-400 transition-colors ease-out space-x-1 group ${active && 'text-blue-400'}` }>
                 <Icon className='group-hover:animate-bounce' height="18" width="18" />
                 <p>{ text }</p>
             </div>
