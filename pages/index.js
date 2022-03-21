e00d23c5abbf66d7b8e93d19c80053a506184cc4
JethroSama <kel73896@gmail.com>
2022-03-21 17:24:19 +0800

    add heroimage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import HeroImage from '../components/HeroImage'
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       {/* Header */ }
       <Header />
-
+      <HeroImage />
 
     </div>
   )
