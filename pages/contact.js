995e5ad9a5e09269cbccebc77edada9f484f0d7b
JethroSama <kel73896@gmail.com>
2022-03-22 19:02:51 +0800

    add header in contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Contact = () => {
     return (
-        <div>Contact me bruh</div>
+        <div className='min-h-screen font-mono text-gray-800'>
+            <Header />
+        </div>
     )
 }
 
