412b937336c1b9aad94faf1cf3cb8b7ca047bd4f
JethroSama <kel73896@gmail.com>
2022-03-22 17:51:21 +0800

    add notion sdk

--- pages/music-sheets.js
+++ pages/music-sheets.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Header from '../components/Header'
 
+const musics = [
+    { title: 'Attack on Titan Season 4 OST - Ashes on the Fire', id: '1', pdf: 'https://www.notion.so/Music-sheets-cbaead488e29421b897ba4293840adfc' },
+]
 const MusicSheets = () => {
     return (
         <div className='min-h-screen font-mono'>
             <Header />
+            {/* Headings */ }
+            <div>
+                {/* Number */ }
+                {/* Title */ }
 
+
+            </div>
         </div>
     )
 }
