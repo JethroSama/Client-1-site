a33a42d7833920f9059e227209d54ca9581ecd0d
JethroSama <kel73896@gmail.com>
2022-03-22 18:26:01 +0800

    create query for music sheets

--- pages/music-sheets.js
+++ pages/music-sheets.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
+import { getMusicSheets } from '../lib/notionDb'
 
-const musics = [
-    { title: 'Attack on Titan Season 4 OST - Ashes on the Fire', id: '1', pdf: 'https://www.notion.so/Music-sheets-cbaead488e29421b897ba4293840adfc' },
-]
-const MusicSheets = () => {
+export async function getStaticProps() {
+    const data = await getMusicSheets()
+    console.log(data)
+    return {
+        props: {
+            data
+        },
+    }
+}
+const MusicSheets = ({ data }) => {
+    console.log(data)
     return (
         <div className='min-h-screen font-mono'>
             <Header />
