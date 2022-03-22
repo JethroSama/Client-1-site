99a36a924c7ab6958890b3acd00e7ff5bbd867e2
JethroSama <kel73896@gmail.com>
2022-03-22 19:27:44 +0800

    contact page edit

--- pages/contact.js
+++ pages/contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
         <div className='min-h-screen font-mono text-gray-800'>
             <Header />
             <h1 className='text-2xl mt-2 text-center'>Contact me:</h1>
-            <div className='flex'>
+            <div className='flex justify-center items-center'>
                 <SocialIcon image={ facebookIcon } url="https://www.facebook.com/azeriru.01" />
                 <SocialIcon image={ youtubeIcon } url="https://www.youtube.com/c/AzerirusMusicCover" />
                 <SocialIcon image={ discordIcon } url="https://discord.com/invite/v7xn7v" />
