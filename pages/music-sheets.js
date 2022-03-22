55f0abe7364b1aa74078720f7a50ea3327c47355
JethroSama <kel73896@gmail.com>
2022-03-22 18:48:14 +0800

    style table item

--- pages/music-sheets.js
+++ pages/music-sheets.js
@@ -17,19 +17,16 @@ const MusicSheets = ({ data }) => {
         <div className='min-h-screen font-mono text-gray-800'>
             <Header />
             {/* Headings */ }
-            <div className='mt-5 shadow-sm px-2'>
-                <div className='shadow-sm'>
+            <div className='mt-5 shadow-sm border-2 border-slate-50 rounded-lg max-w-xl mx-auto'>
+                <div className='shadow-sm px-2'>
                     <p>Title</p>
                 </div>
                 <div>
-                    <div className='flex group cursor-pointer justify-between items-center shadow-sm hover:shadow-md h-14 transition-all ease-out hover:bg-gray-50'>
+                    <div className='flex group cursor-pointer justify-between items-center shadow-sm hover:shadow-md h-14 transition-all ease-out hover:bg-gray-50 px-2'>
                         <p className='text-sm'>Test1</p>
-                        <button className='hidden group-hover:block border-2 rounded-lg p-1 hover:border-blue-300 hover:text-blue-400 transition-all ease-out '>Download</button>
+                        <button className='hidden group-hover:block border-2 rounded-lg p-1 hover:border-blue-300 hover:text-blue-400 transition-all ease-out text-sm'>Download</button>
                     </div>
-                    <div className='flex group cursor-pointer justify-between items-center hover:shadow-md h-14 transition-all ease-out hover:bg-gray-50'>
-                        <p className='text-sm'>Test1</p>
-                        <button className='hidden group-hover:block border-2 rounded-lg p-1 hover:border-blue-300 hover:text-blue-400 transition-all ease-out '>Download</button>
-                    </div>
+
                 </div>
             </div>
         </div>
