93c78bebb92284327dc7cd4240386ef093987c11
JethroSama <kel73896@gmail.com>
2022-03-22 19:31:33 +0800

    fix error

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
 
     return (
         <div className='md:flex items-center space-y-3 md:space-y-0 bg-white py-3 shadow-md'>
-            <div className='justify-center flex items-center space-x-1 text-lg md:text-xl md:pl-10'><MusicNoteIcon height="20" width="20" /><h1>Azeriru's Music Library</h1> </div>
+            <div className='justify-center flex items-center space-x-1 text-lg md:text-xl md:pl-10'><MusicNoteIcon height="20" width="20" /><h1>{ "Azeriru's Music Library" }</h1> </div>
 
             <div className='flex text-sm md:text-md flex-1 space-x-10 justify-center'>
                 {/* About */ }
