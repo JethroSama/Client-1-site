fecb38c35b78e707ad11a2961310819e0d090573
JethroSama <kel73896@gmail.com>
2022-03-22 18:41:15 +0800

    add and style table

--- pages/music-sheets.js
+++ pages/music-sheets.js
@@ -14,14 +14,23 @@ export async function getStaticProps() {
 const MusicSheets = ({ data }) => {
     console.log(data)
     return (
-        <div className='min-h-screen font-mono'>
+        <div className='min-h-screen font-mono text-gray-800'>
             <Header />
             {/* Headings */ }
-            <div>
-                {/* Number */ }
-                {/* Title */ }
-
-
+            <div className='mt-5 shadow-sm px-2'>
+                <div className='shadow-sm'>
+                    <p>Title</p>
+                </div>
+                <div>
+                    <div className='flex group cursor-pointer justify-between items-center shadow-sm hover:shadow-md h-14 transition-all ease-out hover:bg-gray-50'>
+                        <p className='text-sm'>Test1</p>
+                        <button className='hidden group-hover:block border-2 rounded-lg p-1 hover:border-blue-300 hover:text-blue-400 transition-all ease-out '>Download</button>
+                    </div>
+                    <div className='flex group cursor-pointer justify-between items-center hover:shadow-md h-14 transition-all ease-out hover:bg-gray-50'>
+                        <p className='text-sm'>Test1</p>
+                        <button className='hidden group-hover:block border-2 rounded-lg p-1 hover:border-blue-300 hover:text-blue-400 transition-all ease-out '>Download</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
